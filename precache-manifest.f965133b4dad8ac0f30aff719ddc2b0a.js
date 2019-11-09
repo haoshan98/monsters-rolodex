0835e4a2f9ca2d1a5c6c36b884743fa2dcc1a2b2
hao shan <haoshan9898@gmail.com>
2019-11-10 01:11:08 +0800

    Updates

---
+++ precache-manifest.f965133b4dad8ac0f30aff719ddc2b0a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8d7de0f1e120d031a7d4b509ae7306c",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "c4f204c3d86e3482047f",
+    "url": "/monsters-rolodex/static/css/main.7cc0c399.chunk.css"
+  },
+  {
+    "revision": "30de84241a4b7e152e8e",
+    "url": "/monsters-rolodex/static/js/2.80469be7.chunk.js"
+  },
+  {
+    "revision": "c4f204c3d86e3482047f",
+    "url": "/monsters-rolodex/static/js/main.da68b9f9.chunk.js"
+  },
+  {
+    "revision": "2dc9b40a277f725e4d87",
+    "url": "/monsters-rolodex/static/js/runtime-main.b05ca383.js"
+  }
+]);
